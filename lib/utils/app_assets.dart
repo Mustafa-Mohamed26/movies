f830de5e0f6b0c0bc873c5648d6c56759f0ce551
Mustafa-Mohamed26 <mustafamohameda447@gmail.com>
2025-08-20 23:57:49 +0300

    + adding a home screen with a custom bottom navigation bar

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,6 @@
+class AppAssets {
+  static const String homeIcon = "assets/home_icon.png";
+  static const String searchIcon = "assets/search_icon.png";
+  static const String browseIcon = "assets/browse_icon.png";
+  static const String profileIcon = "assets/profile_icon.png";
+}
