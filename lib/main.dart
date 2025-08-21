8d676de5aa2873e9437a45c74b570f4326c99d99
anas <hans16486@gmail.com>
2025-08-21 21:56:15 +0300

    feat splash screen + on boarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,12 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:movies/on%20boarding/first-on-boarding.dart';
+import 'package:movies/on%20boarding/on_boarding.dart';
 import 'package:movies/ui/auth/forgot_password_screen.dart';
 import 'package:movies/ui/auth/login_screen.dart';
 import 'package:movies/ui/auth/register_screen.dart';
 import 'package:movies/ui/home/home_screen.dart';
 import 'package:movies/ui/home/profile_screen.dart';
-import 'package:movies/ui/onboarding/onboarding_screen_1.dart';
-import 'package:movies/ui/onboarding/onboarding_screen_2.dart';
 import 'package:movies/utils/app_routes.dart';
 import 'package:movies/utils/app_theme.dart';
 
@@ -21,14 +21,14 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      initialRoute: AppRoutes.home,
+
+      initialRoute: AppRoutes.onboarding1,
       routes: {
         AppRoutes.login: (context) => const LoginScreen(),
         AppRoutes.register: (context) => const RegisterScreen(),
         AppRoutes.forgotPassword: (context) =>  ForgotPasswordScreen(),
-        AppRoutes.onboarding1: (context) => const OnboardingScreen1(),
-        AppRoutes.onboarding2: (context) => const OnboardingScreen2(),
+        AppRoutes.onboarding1: (context) => const FirstOnBoarding(),
+        AppRoutes.onboarding2: (context) => const OnBoarding(),
         AppRoutes.home: (context) =>  HomeScreen(),
         AppRoutes.profile: (context) => const ProfileScreen(),
       },
