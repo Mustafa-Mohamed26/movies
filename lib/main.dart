0ccd06e216dce265c40bef04728bfcdd7499b898
anas <hans16486@gmail.com>
2025-08-21 21:16:14 +0300

    feat splash screen + on boarding

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,10 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:movies/ui/auth/forgot_password_screen.dart';
-import 'package:movies/ui/auth/login_screen.dart';
-import 'package:movies/ui/auth/register_screen.dart';
-import 'package:movies/ui/home/home_screen.dart';
-import 'package:movies/ui/home/profile_screen.dart';
-import 'package:movies/ui/onboarding/onboarding_screen_1.dart';
-import 'package:movies/ui/onboarding/onboarding_screen_2.dart';
+import 'package:movies/on%20boarding/first-on-boarding.dart';
 import 'package:movies/utils/app_routes.dart';
-import 'package:movies/utils/app_theme.dart';
+import 'on boarding/on_boarding.dart';
 
 void main() {
-  runApp(MyApp());
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -21,20 +14,12 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      
-      initialRoute: AppRoutes.home,
+      initialRoute: AppRoutes.firstOnBoarding,
       routes: {
-        AppRoutes.login: (context) => const LoginScreen(),
-        AppRoutes.register: (context) => const RegisterScreen(),
-        AppRoutes.forgotPassword: (context) =>  ForgotPasswordScreen(),
-        AppRoutes.onboarding1: (context) => const OnboardingScreen1(),
-        AppRoutes.onboarding2: (context) => const OnboardingScreen2(),
-        AppRoutes.home: (context) =>  HomeScreen(),
-        AppRoutes.profile: (context) => const ProfileScreen(),
+        AppRoutes.firstOnBoarding: (context) => FirstOnBoarding(),
+        AppRoutes.onBoarding: (context) => OnBoarding(),
       },
-
-      darkTheme: AppTheme.darkTheme,
-      themeMode: ThemeMode.dark,
     );
   }
 }
+
--- lib/utils/app_assets.dart
+++ lib/utils/app_assets.dart
@@ -1,7 +1,8 @@
 class AppAssets {
-  static const String homeIcon = "assets/icons/home_icon.png";
-  static const String searchIcon = "assets/icons/search_icon.png";
-  static const String browseIcon = "assets/icons/browse_icon.png";
-  static const String profileIcon = "assets/icons/profile_icon.png";
-  static const String forgotPassword = 'assets/images/Forgot_password.png';
+  static const String firstOnBoarding = 'assets/images/movies-posters.png';
+  static const String onBoarding1 = 'assets/images/OnBoarding_1.png';
+  static const String onBoarding2 = 'assets/images/OnBoarding_2.png';
+  static const String onBoarding3 = 'assets/images/OnBoarding_3.png';
+  static const String onBoarding4 = 'assets/images/OnBoarding_4.png';
+  static const String onBoarding5 = 'assets/images/OnBoarding_5.png';
 }
