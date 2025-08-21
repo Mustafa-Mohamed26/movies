f001e451097f8dcae31e3cd841e307baf0cccc9c
anas <hans16486@gmail.com>
2025-08-21 19:27:08 +0300

    commit added splash screen + on boarding screen

---
+++ lib/utils/app_assets.dart
@@ -0,0 +1,8 @@
+class AppAssets {
+  static const String firstOnBoarding = 'assets/images/movies-posters.png';
+  static const String onBoarding1 = 'assets/images/OnBoarding_1.png';
+  static const String onBoarding2 = 'assets/images/OnBoarding_2.png';
+  static const String onBoarding3 = 'assets/images/OnBoarding_3.png';
+  static const String onBoarding4 = 'assets/images/OnBoarding_4.png';
+  static const String onBoarding5 = 'assets/images/OnBoarding_5.png';
+}
